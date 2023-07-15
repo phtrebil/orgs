0e3018c5d4df5148595979027116955ac78ef99b
phtrebil <phptrebil@gmail.com>
2023-07-15 09:34:37 -0300

    criando teste para preencher formulario e salvar o produto

--- app/src/androidTest/java/br/com/alura/orgs/ui/activity/ProdutoActivityTests.kt
+++ app/src/androidTest/java/br/com/alura/orgs/ui/activity/ProdutoActivityTests.kt
@@ -2,6 +2,7 @@ package br.com.alura.orgs.ui.activity
 
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import br.com.alura.orgs.R
@@ -22,5 +23,14 @@ class ProdutoActivityTests {
         onView(withId(R.id.activity_formulario_produto_descricao)).check(matches(isDisplayed()))
         onView(withId(R.id.activity_formulario_produto_botao_salvar)).check(matches(isDisplayed()))
         onView(withId(R.id.activity_formulario_produto_valor)).check(matches(isDisplayed()))
+    }
+
+    @Test fun deveSerCapazDePreencherFormularioESalvar(){
+        launch(FormularioProdutoActivity::class.java)
+        onView(withId(R.id.activity_formulario_produto_nome)).perform(ViewActions.typeText("Banana"), ViewActions.pressBack())
+        onView(withId(R.id.activity_formulario_produto_descricao)).perform(ViewActions.typeText("Banana Prata"), ViewActions.pressBack())
+        onView(withId(R.id.activity_formulario_produto_valor)).perform(ViewActions.typeText("6.99"), ViewActions.pressBack())
+        onView(withId(R.id.activity_formulario_produto_botao_salvar)).perform(ViewActions.click())
+
     }
 }
