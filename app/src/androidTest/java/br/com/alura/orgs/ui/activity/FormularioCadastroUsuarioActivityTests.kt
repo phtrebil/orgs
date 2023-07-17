2ffbd28078bcbc10951b017277b63380e8730b98
phtrebil <phptrebil@gmail.com>
2023-07-17 09:30:26 -0300

    inplementando teste instrumentado no Cadastro de Usuarios

--- app/src/androidTest/java/br/com/alura/orgs/ui/activity/FormularioCadastroUsuarioActivityTests.kt
+++ app/src/androidTest/java/br/com/alura/orgs/ui/activity/FormularioCadastroUsuarioActivityTests.kt
@@ -2,6 +2,7 @@ package br.com.alura.orgs.ui.activity
 
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -20,5 +21,18 @@ class FormularioCadastroUsuarioActivityTests{
             .check(ViewAssertions.matches(isDisplayed()))
         onView(withId(R.id.activity_formulario_cadastro_botao_cadastrar))
             .check(ViewAssertions.matches(isDisplayed()))
+    }
+
+    @Test
+    fun devePreencherOsCamposNecessariosParaFazerOCadastro(){
+        launch(FormularioCadastroUsuarioActivity::class.java)
+        onView(withId(R.id.activity_formulario_cadastro_usuario))
+            .perform(ViewActions.typeText("phtrebil"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.activity_formulario_cadastro_nome))
+            .perform(ViewActions.typeText("Pedro Trebilcock"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.activity_formulario_cadastro_senha))
+            .perform(ViewActions.typeText("1234"), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.activity_formulario_cadastro_botao_cadastrar))
+            .perform(ViewActions.click())
     }
 }
